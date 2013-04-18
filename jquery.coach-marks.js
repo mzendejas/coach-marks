1dbbbea9040607ddcc64e766676d80322512c39c
Manuel <0degreez@gmail.com>
2013-04-18 02:52:01 -0500

    with parentClassList

--- jquery.coach-marks.js
+++ jquery.coach-marks.js
@@ -64,8 +64,8 @@
 				{
 					var parentClassList = $e.parent().attr('class').replace(/ui-/g, "cm-");
 					$copy = $e.clone();
-					//' + (typeof parentClassList !== "undefined") ? ' ' + parentClassList : '' + '
-					$copy = this._deleteData( $copy ).attr('id', 'coach-element-' + this.coach_elements).wrap('<div class="coach-element" style="position:absolute;z-index:2010;" />').parent().css( { top: $e_pos.top + 'px', left: $e_pos.left + 'px' } );
+					//
+					$copy = this._deleteData( $copy ).attr('id', 'coach-element-' + this.coach_elements).wrap('<div class="coach-element' + ( ( typeof parentClassList !== "undefined") ? " " + parentClassList : "" ) + '" style="position:absolute;z-index:2010;" />').parent().css( { top: $e_pos.top + 'px', left: $e_pos.left + 'px' } );
 					this.$main_div.find('div#coach-elements').append( $copy );
 					obj.element.cloned = $copy;
 				}
